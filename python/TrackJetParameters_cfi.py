f3b78f9cfb9f8cf5bcb059d635369b6437115f45
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2009-12-10 16:45:33 +0000

    Update for full TrackJet recipe.

--- python/TrackJetParameters_cfi.py
+++ python/TrackJetParameters_cfi.py
@@ -1,23 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 TrackJetParameters = cms.PSet(
-    src            = cms.InputTag("trackRefsForJets"),
-    srcPVs         = cms.InputTag(''),
+    src            = cms.InputTag('trackRefsForJets'),
+    srcPVs         = cms.InputTag('offlinePrimaryVertices'),
     jetType        = cms.string('TrackJet'),
-    jetPtMin       = cms.double(1.0),
+    jetPtMin       = cms.double(0.3),
     inputEMin      = cms.double(0.0),
     inputEtMin     = cms.double(0.0),
-    doPVCorrection = cms.bool(False),
-    # pileup with offset correction
-    doPUOffsetCorr = cms.bool(False),
-       # if pileup is false, these are not read:
-       nSigmaPU = cms.double(1.0),
-       radiusPU = cms.double(0.5),  
-    # fastjet-style pileup     
-    doPUFastjet    = cms.bool(False),
-      # if doPU is false, these are not read:
-      Active_Area_Repeats = cms.int32(5),
-      GhostArea = cms.double(0.01),
-      Ghost_EtaMax = cms.double(6.0)
-    )
-
+    doPVCorrection = cms.bool(False), # not used
+    doPUOffsetCorr = cms.bool(False), # not used
+    doPUFastjet    = cms.bool(False), # not used
+    # only use the tracks that were used to fit the vertex
+    UseOnlyVertexTracks = cms.bool(False),
+    # only consider the highest-sum-pT PV for clustering
+    UseOnlyOnePV        = cms.bool(True),
+    # maximum z-distance bweteen track and vertex for association (in cm)
+    DzTrVtxMax          = cms.double(0.5)
+)
