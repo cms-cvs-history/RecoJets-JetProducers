755fcc7a0802bfb493fea777d4cce63978e6f3a5
cmsbuild <>
2009-12-11 19:50:55 +0000

    This commit was manufactured by cvs2git to create branch 'RecoJets- JetProducers-LowetteTrackJet_100107'.
    Sprout from master 2009-12-11 19:50:50 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Add track jets completed implementation'
    Delete:
        BuildFile
        doc/html/index.html
        doc/html/jetSimuChain.html
        doc/html/overview.html
        interface/JetSpecific.h
        plugins/BuildFile
        plugins/CATopJetProducer.cc
        plugins/CATopJetProducer.h
        plugins/CMSInsideOutJetProducer.cc
        plugins/CMSInsideOutJetProducer.h
        plugins/CompoundJetProducer.cc
        plugins/CompoundJetProducer.h
        plugins/InputGenJetsParticleSelector.cc
        plugins/InputGenJetsParticleSelector.h
        plugins/JetIDProducer.cc
        plugins/JetIDProducer.h
        plugins/PtMinJetSelector.cc
        plugins/PtMinJetSelector.h
        plugins/SubEventGenJetProducer.cc
        plugins/SubEventGenJetProducer.h
        plugins/SubJetProducer.cc
        plugins/SubJetProducer.h
        plugins/VirtualJetProducer.cc
        plugins/VirtualJetProducer.h
        python/AnomalousCellParameters_cfi.py
        python/CATopJetParameters_cfi.py
        python/CaloJetParameters_cfi.py
        python/CaloTowerSchemeBWithHO_cfi.py
        python/CaloTowerSchemeB_cfi.py
        python/CaloTowerSchemeBnoEE_cfi.py
        python/FastjetParameters_cfi.py
        python/GenJetParameters_cfi.py
        python/JetIDParams_cfi.py
        python/PFJetParameters_cfi.py
        python/SubJetParameters_cfi.py
        python/__init__.py
        python/ak5CaloJets_cfi.py
        python/ak5GenJets_cfi.py
        python/ak5JetID_cfi.py
        python/ak5PFJets_cfi.py
        python/ak5TrackJets_cfi.py
        python/ak7JetID_cfi.py
        python/ca4CaloJets_cfi.py
        python/ca4GenJets_cfi.py
        python/ca4JetID_cfi.py
        python/ca4PFJets_cfi.py
        python/ca4TrackJets_cfi.py
        python/ca6JetID_cfi.py
        python/gk5CaloJets_cfi.py
        python/gk5GenJets_cfi.py
        python/gk5JetID_cfi.py
        python/gk5PFJets_cfi.py
        python/gk5TrackJets_cfi.py
        python/gk7JetID_cfi.py
        python/ic5CaloJets_cfi.py
        python/ic5GenJets_cfi.py
        python/ic5JetID_cfi.py
        python/ic5PFJets_cfi.py
        python/ic5TrackJets_cfi.py
        python/ic7JetID_cfi.py
        python/kt4CaloJets_cfi.py
        python/kt4GenJets_cfi.py
        python/kt4JetID_cfi.py
        python/kt4PFJets_cfi.py
        python/kt4TrackJets_cfi.py
        python/kt6JetID_cfi.py
        python/modify_text.pl
        python/sc5CaloJets_cfi.py
        python/sc5GenJets_cfi.py
        python/sc5JetID_cfi.py
        python/sc5PFJets_cfi.py
        python/sc5TrackJets_cfi.py
        python/sc7JetID_cfi.py
        src/JetSpecific.cc
        test/analyzeJets.C
        test/analyzeJetsChain.C
        test/analyzeJets_head.C
        test/analyzeJets_headChain.C
        test/prepareCompile.C
        test/runJetReco_cfg.py
        validation/README
        validation/fastjetJetProducer_validation_cfg.py
        validation/jetProducers_validation_cfg.py
        validation/validation.sh

--- plugins/PtMinJetSelector.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "PtMinJetSelector.h"
-
-DEFINE_ANOTHER_FWK_MODULE(PtMinCaloJetSelector);
-DEFINE_ANOTHER_FWK_MODULE(PtMinGenJetSelector);
-DEFINE_ANOTHER_FWK_MODULE(PtMinPFJetSelector);
-DEFINE_ANOTHER_FWK_MODULE(PtMinBasicJetSelector);
