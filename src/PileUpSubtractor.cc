0b152d770e263a7ccdcf53313912d66f73c9ee90
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-06-22 09:30:30 +0000

    compiler warning removed

--- src/PileUpSubtractor.cc
+++ src/PileUpSubtractor.cc
@@ -219,10 +219,6 @@ void PileUpSubtractor::calculateOrphanInput(vector<fastjet::PseudoJet> & orphanI
   for (; pseudojetTMP != fjJetsEnd ; ++pseudojetTMP) {
 
     vector<fastjet::PseudoJet> newtowers;
-      
-    // get eta, phi of this jet
-    double eta2 = pseudojetTMP->eta();
-    double phi2 = pseudojetTMP->phi();
     // find towers within radiusPU_ of this jet
     for(vector<HcalDetId>::const_iterator im = allgeomid_.begin(); im != allgeomid_.end(); im++)
       {
