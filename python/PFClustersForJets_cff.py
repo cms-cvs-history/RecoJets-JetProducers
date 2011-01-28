9f7c797cd0fbb2c85f555285fc692eb3dc3e9713
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2011-01-28 12:47:20 +0000

    produce candidates for HCAL,ECAL,HFEM, and HFHAD pf clusters and one merged collection

--- python/PFClustersForJets_cff.py
+++ python/PFClustersForJets_cff.py
@@ -2,7 +2,28 @@ import FWCore.ParameterSet.Config as cms
 
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
-pfClusterRefsForJets = cms.EDProducer("PFClusterRefCandidateProducer",
+pfClusterRefsForJetsHCAL = cms.EDProducer("PFClusterRefCandidateProducer",
     src          = cms.InputTag('particleFlowClusterHCAL'),
     particleType = cms.string('pi+')
 )
+
+pfClusterRefsForJetsECAL = cms.EDProducer("PFClusterRefCandidateProducer",
+    src          = cms.InputTag('particleFlowClusterECAL'),
+    particleType = cms.string('pi+')
+)
+
+pfClusterRefsForJetsHFEM = cms.EDProducer("PFClusterRefCandidateProducer",
+    src          = cms.InputTag('particleFlowClusterHFEM'),
+    particleType = cms.string('pi+')
+)
+
+pfClusterRefsForJetsHFHAD = cms.EDProducer("PFClusterRefCandidateProducer",
+    src          = cms.InputTag('particleFlowClusterHFHAD'),
+    particleType = cms.string('pi+')
+)
+
+pfClusterRefsForJets = cms.EDProducer("PFClusterRefCandidateMerger",
+    src = cms.VInputTag("pfClusterRefsForJetsHCAL", "pfClusterRefsForJetsECAL","pfClusterRefsForJetsHFEM","pfClusterRefsForJetsHFHAD")
+)
+
+
